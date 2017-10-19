04dcdf6948b37514fe72e4abc404b1b0b94e8a9e
Gabi Beyer <gabrielle.n.beyer@intel.com>
2017-10-19 14:52:00 -0700

    update order and comments for test cases
    Fix comments and move inherit tests up
    
    Signed-off-by: Gabi Beyer <gabrielle.n.beyer@intel.com>

--- tests/test_bb_parser.py
+++ tests/test_bb_parser.py
@@ -23,6 +23,26 @@ class TestParseBitBakeFile(unittest.TestCase):
         expect = "8.0.0983"
         self.assertEqual(expect, bb_parser.scrape_version(os.path.basename(bb_file)))
 
+    def test_scrape_inherits_htop(self):
+        """
+        Test that the package inherits are correctly scraped as a list with
+        only one line/inherit
+        """
+        bb_file = os.path.join('tests', 'testfiles', 'bb', 'htop_1.0.3.bb')
+        bb_dict = bb_parser.bb_scraper(bb_file, None)
+        expect = ["autotools"]
+        self.assertEqual(expect, bb_dict.get('inherits'))
+
+    def test_scrape_inherits_vim(self):
+        """
+        Test that the package inherits are correctly scraped as a list with
+        multiple lines/inherits
+        """
+        bb_file = os.path.join('tests', 'testfiles', 'bb',
+                               'vim_8.0.0983.bb')
+        bb_dict = bb_parser.bb_scraper(bb_file, None)
+        expect = ["autotools update-alternatives", "autotools-brokensep"]
+        self.assertEqual(expect, bb_dict.get('inherits'))
 
     # def test_scrape_summary_htop(self):
     #     """
@@ -61,28 +81,6 @@ class TestParseBitBakeFile(unittest.TestCase):
     #     expect = "GPLv2"
     #     self.assertEqual(expect,
     #             bb_parser.bb_dict.get('license'))
-
-
-    def test_scrape_inherits_htop(self):
-        """
-        Test that the package inherits are correctly scraped as a list
-        """
-        bb_file = os.path.join('tests', 'testfiles', 'bb', 'htop_1.0.3.bb')
-        bb_dict = bb_parser.bb_scraper(bb_file, None)
-        expect = ["autotools"]
-        self.assertEqual(expect, bb_dict.get('inherits'))
-
-
-    def test_scrape_inherits_vim(self):
-        """
-        Test that the package inherits are correctly scraped as a list
-        """
-        
-        bb_file = os.path.join('tests', 'testfiles', 'bb', 'vim_8.0.0983.bb')
-        bb_dict = bb_parser.bb_scraper(bb_file, None)
-        expect = ["autotools update-alternatives", "autotools-brokensep"]
-        self.assertEqual(expect, bb_dict.get('inherits'))
-
 
     # def test_scrape_lic_files_chksum_htop_double_eq(self):
     #     """
